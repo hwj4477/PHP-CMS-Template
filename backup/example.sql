ce35563e18444abed9cea86959e4bb880ecfce51
hwj4477 <hwj4477@gmail.com>
2015-01-23 17:47:32 +0900

    update sql

--- backup/example.sql
+++ backup/example.sql
@@ -1,51 +1,28 @@
 -- phpMyAdmin SQL Dump
--- version 4.0.6
+-- version 4.2.10
 -- http://www.phpmyadmin.net
 --
--- Host: localhost
--- Generation Time: Mar 19, 2014 at 07:49 AM
--- Server version: 5.5.33
--- PHP Version: 5.5.3
+-- Host: localhost:8889
+-- Generation Time: 15-01-23 09:45
+-- Server Version: 5.5.38
+-- PHP Version: 5.5.18
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
 --
--- Database: `hwj4477`
+-- database: `temp`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `product`
+-- Table `example`
 --
 
-CREATE TABLE `product` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `brand` varchar(50) CHARACTER SET utf8 NOT NULL,
-  `type` varchar(10) CHARACTER SET utf8 NOT NULL,
-  `product_id` varchar(50) CHARACTER SET utf8 NOT NULL,
-  `name` varchar(50) CHARACTER SET utf8 NOT NULL,
-  `recommand` varchar(200) CHARACTER SET utf8 NOT NULL,
-  `price` varchar(30) NOT NULL,
-  `sale` tinyint(4) NOT NULL,
-  `post` tinyint(4) NOT NULL,
-  `try` tinyint(4) NOT NULL,
-  `image_thum` varchar(100) CHARACTER SET utf8 NOT NULL,
-  `image_detail` varchar(100) CHARACTER SET utf8 NOT NULL,
-  `image_try1` varchar(100) CHARACTER SET utf8 NOT NULL,
-  `image_try2` varchar(100) NOT NULL,
-  `video` varchar(100) CHARACTER SET utf8 NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `product_id` (`product_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+CREATE TABLE `example` (
+`id` int(11) NOT NULL,
+  `col1` varchar(50) DEFAULT NULL,
+  `col2` varchar(50) DEFAULT NULL,
+  `col3` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
